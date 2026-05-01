807159bdab667114e62667c8ebc6578af9a37032
Andrey Kozimirov <an.kozimirov@gmail.com>
2026-05-01 22:35:39 +0300

    chore: add tailwind, config eslint

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+
 import './index.css';
+import App from './pages/app';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <h1>React Class Components</h1>
+    <App />
   </StrictMode>
 );
---
+++ src/pages/app.tsx
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <h1>My App</h1>
+        <div className="rounded-lg bg-blue-500 p-6 text-white shadow-lg">
+          Hello world
+        </div>
+      </>
+    );
+  }
+}
+
+export default App;
